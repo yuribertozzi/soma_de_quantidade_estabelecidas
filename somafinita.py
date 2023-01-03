017e00c8446ceaeb8340f8c68189889fb544930b
Yuri Silva <121751241+yuribertozzi@users.noreply.github.com>
2023-01-03 12:18:25 -0300

    Add files via upload

---
+++ somafinita.py
@@ -0,0 +1,22 @@
+print()
+
+tamanho = int(input("Digite a quantidade de números que quer somar: "))
+
+print()
+
+soma = 0
+
+n = 0
+
+while tamanho > n:
+
+	valor = int(input("Digite um valor a ser somado: "))
+
+	soma = soma + valor
+
+	n = n + 1
+
+
+print()
+
+print("A soma dos valores digitados é: ", soma)
